84e20a826e38d33b088962b08e96e4d81369be05
hwang0617 <774286503@qq.com>
2017-05-12 11:59:51 +0800

    mm
    mm

---
+++ maker camp/hwang_files/6597131339307155859.js
@@ -0,0 +1,26 @@
+﻿/* @script.js文件 */
+
+$(document).ready(function () {
+	// layout 
+	if ($.browser.msie && $.browser.version === '10.0') {  // <ul>,<ol>
+		$('html').addClass('ie10');
+	}
+	
+	// search
+	bindSchEvents($('#j-lnksch'), $('#j-schform .txt'));
+});
+function bindSchEvents($lnk, $txt){
+	$lnk.bind('click', function (){
+		$(this).addClass('f-hidden').parent().addClass('m-schshow');
+		setTimeout(function (){
+			$txt.focus();
+		}, 300);
+		return false;
+	});
+	$txt.bind('blur', function (){
+		setTimeout(function (){
+			$lnk.removeClass('f-hidden').parent().removeClass('m-schshow');
+		}, 300);
+		return false;
+	});
+}
